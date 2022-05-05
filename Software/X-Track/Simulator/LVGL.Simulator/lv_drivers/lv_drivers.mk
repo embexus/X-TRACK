c226285aa72239fd0e943a72c6765d9c3973c227
Ayoub Zaki <ayoub.zaki@embexus.com>
2022-05-05 11:37:23 +0200

    fix cross-compilation

--- Software/X-Track/Simulator/LVGL.Simulator/lv_drivers/lv_drivers.mk
+++ Software/X-Track/Simulator/LVGL.Simulator/lv_drivers/lv_drivers.mk
@@ -1,5 +1,10 @@
 LV_DRIVERS_DIR_NAME ?= lv_drivers
 
+override CFLAGS   := -I$(LVGL_DIR) -I$(PROJECT_DIR)/USER/App -I$(PROJECT_DIR)/USER/App/Utils/ArduinoJson/src $(CFLAGS)
+override CXXFLAGS := -I$(LVGL_DIR) -I$(PROJECT_DIR)/USER/App -I$(PROJECT_DIR)/USER/App/Utils/ArduinoJson/src $(CXXFLAGS)
+override LDFLAGS := -lm -lstdc++ $(LDFLAGS)
+
+
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/*.c)
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/wayland/*.c)
 CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/indev/*.c)
